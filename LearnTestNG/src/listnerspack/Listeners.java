4d933b5610e2bca597cdba8cd75acd1854a7052c
abhishek <abhishek@yahoo.com>
2023-04-04 00:58:16 +0530

    listerner

---
+++ LearnTestNG/src/listnerspack/Listeners.java
@@ -0,0 +1,32 @@
+package listnerspack;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+
+		System.out.println("Test started ");
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("Test passed");
+
+	}
+	
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Test failed");
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("Test skipped");
+
+	}
+}
